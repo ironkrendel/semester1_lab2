647c59e0b492816d637a047f886e62c0bac337ee
ironkrendel <v.a.ryshkov@gmail.com>
2024-09-23 10:18:26 +0300

    fix word count

--- filestat.c
+++ filestat.c
@@ -49,11 +49,11 @@ int main(int argc, char** argv) {
 
     while ((c = getchar()) != EOF) {
         if (count_words) {
-            if (c != ' ' && word_state == OUT) {
+            if ((c != ' ') && (word_state == OUT)) {
                 word_count++;
                 word_state = IN;
             }
-            else {
+            else if (c == ' ') {
                 word_state = OUT;
             }
         }
